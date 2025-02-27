ed09c73cf8874c85441d598a05b4da83405686bf
butter1011 <luckybutter322982@gmail.com>
2025-02-28 07:00:26 +0900

    fix: deployment error

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 connectDB();
 
 app.get("/", (req, res) => {
