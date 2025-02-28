473db79fa97bff845a5e83d45351635a2fc935ff
butter1011 <luckybutter322982@gmail.com>
2025-03-01 06:33:10 +0900

    fix: fix the port config

--- index.js
+++ index.js
@@ -579,7 +579,7 @@ async function scanWalletAndUpdateTransaction(
     return updatedTransaction;
   }
 }
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
