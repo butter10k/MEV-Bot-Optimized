64fd31e927d570a1d1eebbdb9ba5863e091ad4a3
butter1011 <luckybutter322982@gmail.com>
2025-03-01 06:12:30 +0900

    fix: fix the error

--- index.js
+++ index.js
@@ -579,7 +579,7 @@ async function scanWalletAndUpdateTransaction(
     return updatedTransaction;
   }
 }
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
